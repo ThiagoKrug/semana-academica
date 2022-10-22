3954495f1b977c4d7f5dae937c2aff94a1b4faa0
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2022-10-22 10:17:20 -0300

    Repetição adicionei

--- index.php
+++ index.php
@@ -3,3 +3,9 @@
 echo "<br>Me chamo Thiago Cassio Krug";
 echo "hoje eu aprendi a usar o git";
 echo"me chamo Arthur";
+$J = 54;
+while (J <= 75){
+    $J ++;
+    echo "o Numero J vale: $J <br>";
+
+}
